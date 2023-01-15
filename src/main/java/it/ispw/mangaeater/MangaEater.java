7f16944369124c5cf6b17ffc6e80967252800948
Davide-57 <davide.valenzisi@gmail.com>
2023-01-15 17:01:45 +0100

    credenziali del db inseriti su persistenza di tipo file system

--- src/main/java/it/ispw/mangaeater/MangaEater.java
+++ src/main/java/it/ispw/mangaeater/MangaEater.java
@@ -7,9 +7,6 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Objects;
 
 public class MangaEater extends Application {
@@ -24,17 +21,6 @@ public class MangaEater extends Application {
         stage.getIcons().add(new Image(Objects.requireNonNull(MangaEater.class.getResourceAsStream("/images/Logo_MangaEater.png"))));
 
         stage.show();
-
-        String url = "jdbc:mysql://localhost:3306/mangaeater";
-        String username = "user";
-
-        System.out.println("Connecting database...");
-
-        try (Connection connection = DriverManager.getConnection(url, username, "user")) {
-            System.out.println("Database connected!");
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
-        }
     }
 
 
--- src/main/java/it/ispw/mangaeater/dao/DbConnection.java
+++ src/main/java/it/ispw/mangaeater/dao/DbConnection.java
@@ -1,5 +1,8 @@
 package it.ispw.mangaeater.dao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,14 +18,24 @@ public class DbConnection {
     public static synchronized Connection getConnection() {
         if (conn == null) {
             try{
-                String user = "user";
+                BufferedReader in = new BufferedReader(new FileReader("src/main/resources/utenze/db.txt"));
+                String user;
+                String password;
+                String line;
+                String[] userPasswordArray;
+                line = in.readLine();
+                userPasswordArray = line.split(" ");
+                user = userPasswordArray[0];
+                password = userPasswordArray[1];
                 String dbUrl = "jdbc:mysql://localhost:3306/mangaeater";
                 String driverClassName = "com.mysql.cj.jdbc.Driver";
                 Class.forName(driverClassName);
-                conn = DriverManager.getConnection(dbUrl, user, "user");
+                conn = DriverManager.getConnection(dbUrl, user, password);
             }
             catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
         return conn;
