e88e029c3bccfb3fc81d3a6591e8c057e6e3ae0e
Davide-57 <davide.valenzisi@gmail.com>
2023-02-05 18:09:05 +0100

    Inserito DAOJDBC per gli utenti. Iniziato caso d'uso Pagamento e create due Eccezioni personalizzate ancora da utilizzare nel pagamento

--- src/main/java/it/ispw/mangaeater/dao/query/QueryUtente.java
+++ src/main/java/it/ispw/mangaeater/dao/query/QueryUtente.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 
 public class QueryUtente {
 
-    public QueryUtente() {
+    private QueryUtente() {
         throw new IllegalStateException("Utility class");
     }
 
