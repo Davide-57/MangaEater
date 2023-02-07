cff620bb914b7acb3c29743bb29b2cc0eff55eed
Davide-57 <davide.valenzisi@gmail.com>
2023-02-07 23:22:58 +0100

    Iniziata GUI pagamento.

---
+++ src/main/java/it/ispw/mangaeater/controller_grafici/PagamentoCPController.java
@@ -0,0 +1,48 @@
+package it.ispw.mangaeater.controller_grafici;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class PagamentoCPController {
+
+    @FXML
+    private Button confermaButton;
+
+    @FXML
+    private Text costo;
+
+    @FXML
+    private Text emailVend;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private ImageView imgBackDettaglio;
+
+    @FXML
+    private Text saldo;
+
+    @FXML
+    private Label titolo;
+
+    @FXML
+    void backHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void confermaPagamento(MouseEvent event) {
+
+    }
+
+    @FXML
+    void procediAlPagamento(MouseEvent event) {
+
+    }
+
+}
